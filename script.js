dcde7b45a36b3568c2ffed21bc2b42afd4d950f9
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-02-15 17:54:02 -0800

    SGT v.1 - nick

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var student_array = [];
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
  */
-var inputIds = [];
+var inputIds = ['studentName', 'course', 'studentGrade'];
 
 /**
  * addClicked - Event Handler when user clicks the add button
@@ -19,7 +19,6 @@ var inputIds = [];
 function addClicked()
 {
     addStudent();//adds student object to student_array
-
 }
 
 /**
@@ -39,13 +38,19 @@ function addStudent()
 {
     var new_student = {name: $('#studentName').val(), course: $('#course').val(), grade: $('#studentGrade').val()};
     student_array.push(new_student);
+    clearAddStudentForm();
     return;
 }
-
 
 /**
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
+function clearAddStudentForm()
+{
+    $('#studentName').text('');
+    $('#course').text('');
+    $('#studentGrade').text('');
+}
 
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
@@ -60,11 +65,13 @@ function addStudent()
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
 
+
 /**
  * addStudentToDom - take in a student object, create html elements from the values and then append the elements
  * into the .student_list tbody
  * @param studentObj
  */
+
 
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state

a55157ec816066c233af5e55a428072449da9362
Jaison Amos <jaisonamos@gmail.com>
2016-02-23 10:50:33 -0800

    working on ajax call

--- script.js
+++ script.js
@@ -305,11 +305,26 @@ function auto_complete(){
     console.log("lowest", lowGrade);
     console.log("highest", highGrade);
 }
+
+
+
+
 /**
  * Listen for the document to load and reset the data to the initial state
  */
 $(document).ready(function () {
     updateData();
     gradesHighLow();
+
+
+    $('button').click(function () {
+        console.log('click initiated');
+        $.ajax({
+            dataType: 'json',
+            url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
+            success: function (result) {
+                console.log('AJAX Success function called, with the following result:', result);
+
+                global_result = result;
      //reset();
 });
